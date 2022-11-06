862c920dbd4dbfa1e91dc764ac6c8b476885b939
drewmeekins <drew.a.meekins@gmail.com>
2022-11-06 11:51:46 -0500

    I can console.log my backend data from frontend server
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const ctrls = require('../controllers')
 
 // profile routes
-// router.get('/', ctrls.profile.index) ----> don't really need this
+router.get('/', ctrls.profile.index) // ----> don't really need this
 router.get('/:id', ctrls.profile.getById)
 router.post('/:id', ctrls.profile.create)
 router.delete('/:id', ctrls.profile.destroy)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,33 @@ backendURL = `${process.env.REACT_APP_BACKEND_URL}`
 
 const App = () => {
   console.log(backendURL)
+
+  
+
+  const getProfiles = () => {
+    fetch(backendURL + '/profile')
+    .then((res) => {
+      if(res.status === 200) {
+        return res.json()
+      }else{
+        return[]
+      }
+    })
+    .then((data) => {
+      console.log('data', data)
+      if(data === []) {
+        this.setState({ profiles: data})
+      }else{
+        this.setState({profiles: data.profiles})
+      }
+    })
+  }
+
   return(
     <div>
       <h1>Welcome To FLATSCALE!</h1>
       <h3>{backendURL}</h3>
+      <h5>{getProfiles()}</h5>
     </div>
   )
 }
