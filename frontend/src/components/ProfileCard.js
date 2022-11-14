8d2e21af59c39966ee12b3f30b062af4358072c2
drewmeekins <drew.a.meekins@gmail.com>
2022-11-14 17:12:06 -0500

    have to figure out why db objects are infinitely looping
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,16 +1,47 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 
 
 const ProfileCard = () => {
-    const[profile, setProfile] = useState(0)
+    const[profile, setProfile] = useState({})
+    
+    useEffect(() => {
+        const getProfiles = async () => {
+            const profiles = await fetch(`${process.env.REACT_APP_BACKEND_URL}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                } 
+            })
+            .then(res => res.json())
+            .then(profile => setProfile(profile.profiles))
+            .then((resJson) => {
+                console.log(profile)
+            })
+        }
+        getProfiles()
+    })
+    
 
-    const showProfile = () => {
-
-    }
+    // const loadProfile = () => {
+    //     fetch(`${process.env.REACT_APP_BACKEND_URL}`, {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         } 
+    //     })
+    //     .then((res) => res.json())
+    //     .then((resJson) => {
+    //         console.log('profiles:', profile)
+    //     })
+    //     .catch((err) => {
+    //         console.log(err)
+    //     })
+    // }
 
     
         return(
             <div>
+                <h1>{profile.name}</h1>
                 <h1>Profile</h1>
                 <h4>Showing Profile: </h4>
             </div>
