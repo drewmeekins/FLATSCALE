3bd741bb44322078164b02422a4c76052bc86abc
drewmeekins <drew.a.meekins@gmail.com>
2022-11-14 16:07:45 -0500

    connecting to backend profiles to frontend
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, useState } from 'react';
+import ProfileCard from './components/ProfileCard'
 
 // import backend from .env
 let backendURL = ''
@@ -6,54 +7,14 @@ backendURL = `${process.env.REACT_APP_BACKEND_URL}`
 // console.log('backend:', backendURL)
 
 // going to use this below if using hooks
-// const App = () => {
-//   // setup useState
-//   const [profile, setProfile] = useState(
-//   console.log(backendURL)
+const App = () => {
+  // setup useState
+  const [profile, setProfile] = useState()
+  console.log(backendURL)
 
 
-//   const getProfiles = () => {
-//     fetch(backendURL + '/profile')
-//     .then((res) => {
-//       if(res.status === 200) {
-//         return res.json()
-//       }else{
-//         return[]
-//       }
-//     })
-//     .then((data) => {
-//       console.log('data', data)
-//       if(data === []) {
-//         this.setState({ profiles: data})
-//       }else{
-//         this.setState({profiles: data.profiles})
-//       }
-//     })
-//   }
-
-//   return(
-//     <div>
-//       <h1>Welcome To FLATSCALE!</h1>
-//       <h3>{backendURL}</h3>
-//       <h5>{getProfiles()}</h5>
-//     </div>
-//   )
-// }
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      profiles: []
-    }
-  }
-  
-  componentDidMount() {
-    this.getProfiles()
-  }
-
-  getProfiles() {
-    fetch(backendURL + '/profile')
+  const getProfiles = () => {
+    fetch(backendURL)
     .then((res) => {
       if(res.status === 200) {
         return res.json()
@@ -62,26 +23,25 @@ class App extends Component {
       }
     })
     .then((data) => {
-      console.log('data', data.profiles[0])
+      console.log('data', data)
       if(data === []) {
         this.setState({ profiles: data})
       }else{
-        this.setState({ profiles: data.profiles })
+        this.setState({profiles: data.profiles})
       }
     })
   }
 
-  render() { 
-    // {console.log(this.)}
-    return (
-      <div>
-        <h1>Welcome To FLATSCALE!</h1>
-        <h3>{backendURL}</h3>
-        <h5>{this.state.profiles.username}</h5>
-      </div>
-    );
-  }
+  return(
+    <div>
+      <h1>Welcome To FLATSCALE!</h1>
+      <h3>{backendURL}</h3>
+      <h5>{getProfiles}</h5>
+      <ProfileCard/>
+    </div>
+  )
 }
+
 
 
 export default App
---
+++ frontend/src/components/ProfileCard.js
@@ -0,0 +1,20 @@
+import React, { Component, useState } from 'react'
+
+
+const ProfileCard = () => {
+    const[profile, setProfile] = useState(0)
+
+    const showProfile = () => {
+
+    }
+
+    
+        return(
+            <div>
+                <h1>Profile</h1>
+                <h4>Showing Profile: </h4>
+            </div>
+        )
+}
+
+export default ProfileCard
